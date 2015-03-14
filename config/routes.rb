711558384414406cfb3f441b135f15bec1c47640
gabalicious <gabalicious113@gmail.com>
2015-03-14 11:51:43 -0400

    Edit Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  shallow do
+  resources :events do
+    resources :comments
+    resources :collaborators
+  end
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
